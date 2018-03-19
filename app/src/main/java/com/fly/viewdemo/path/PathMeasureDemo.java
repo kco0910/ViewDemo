f56229a35105c27c939d73143e5d592c2cdde2f4
PC-20151229YDHC\Administrator <523450269@qq.com>
2018-03-19 16:19:26 +0800

    view

--- app/src/main/java/com/fly/viewdemo/path/PathMeasureDemo.java
+++ app/src/main/java/com/fly/viewdemo/path/PathMeasureDemo.java
@@ -1,7 +1,6 @@
 package com.fly.viewdemo.path;
 
 import android.animation.ObjectAnimator;
-import android.animation.TimeInterpolator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -12,6 +11,7 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathMeasure;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -19,8 +19,6 @@ import android.view.animation.AccelerateDecelerateInterpolator;
 
 import com.fly.viewdemo.R;
 import com.orhanobut.logger.Logger;
-
-import dalvik.system.DexClassLoader;
 
 /**
  * 1、参考文章http://blog.csdn.net/u013831257/article/details/51565591
@@ -35,6 +33,7 @@ import dalvik.system.DexClassLoader;
  * Created by Fj on 2018/3/16.
  */
 public class PathMeasureDemo extends View {
+    private static final String TAG = "PathMeasureDemo";
     private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private float animValue;
     private float pos[],tan[];
@@ -57,6 +56,7 @@ public class PathMeasureDemo extends View {
 
     private void init(){
         mPaint.setColor(Color.RED);
+        mPaint.setStyle(Paint.Style.STROKE);
 
         pos = new float[2];
         tan = new float[2];
@@ -69,7 +69,7 @@ public class PathMeasureDemo extends View {
         postDelayed(new Runnable() {
             @Override
             public void run() {
-                aroundFly();
+                aroundFlyAnim();
             }
         },1000);
     }
@@ -107,15 +107,90 @@ public class PathMeasureDemo extends View {
         Logger.i("nextContour 后的pah1长度"+pathMeasure2.getLength());
 
 
+        aroundFly1(canvas);
 
-        //飞机绕圈
-        //通过 tan 得值计算出图片旋转的角度，tan 是 tangent 的缩写，
-        //即中学中常见的正切， 其中tan0是邻边边长，tan1是对边边长，
-        //而Math中 atan2 方法是根据正切是数值计算出该角度的大小,得到的单位是弧度，所以上面又将弧度转为了角度
+        aroundFly2(canvas);
 
-        //第一种实现
+        //testSegment(canvas);
+        search(canvas);
+    }
+
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec),MeasureSpec.getSize(heightMeasureSpec));
+    }
+
+    private void testSegment(Canvas canvas){
+        mPaint.setColor(Color.GREEN);
+        Path path = new Path();
+        path.addRect(-200,-200,200,200, Path.Direction.CW);
+//        canvas.drawPath(path,mPaint);
+        Path dst = new Path();
+        dst.rLineTo(0, 0);//或者关闭硬件加速，否则在<=4.4会导致绘制出问题
+        PathMeasure pathMeasure = new PathMeasure(path,false);
+        pathMeasure.getSegment(0,pathMeasure.getLength()*animValue,dst,true);//true保持原来截取到的path
+        mPaint.setColor(Color.BLACK);
+        canvas.drawPath(dst,mPaint);
+    }
+
+    PathMeasure circleMeasure = new PathMeasure();
+    Path searchPath = new Path();
+    Path circlePath = new Path();
+
+
+    /**
+     * 搜索框动画
+     */
+    private void search(Canvas canvas){
+        mPaint.setColor(Color.GREEN);
+        mPaint.setStrokeWidth(10.0f);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        canvas.translate(-200,-200);
+        //不要使用addCircle方法，这样不能设置开始角度,不方便得到搜索的把手
+        RectF smallRect = new RectF(-50, -50, 50, 50);
+        searchPath.addArc(smallRect,45,359.0f);//不要到360度,否则内部会自动优化,测量不能取到需要的数值
+
+
+        float[] pos = new float[2];
+        RectF bigRect = new RectF(-100, -100, 100, 100);
+        circlePath.addArc(bigRect,45,359.0f);
+        circleMeasure.setPath(circlePath, false);
+        circleMeasure.getPosTan(0, pos, null);
+
+        searchPath.lineTo(pos[0], pos[1]);
+
+
+        circleMeasure.setPath(searchPath,false);
+        float length = circleMeasure.getLength();
+        Path dst = new Path();
+        dst.rLineTo(0,0);
+        boolean segment = circleMeasure.getSegment(length * animValue, length, dst, true);
+        if (segment){
+            canvas.drawPath(dst,mPaint);
+        }else{
+            //开始画转圈
+            circleMeasure.setPath(circlePath,false);
+            length = circleMeasure.getLength();
+            float stopD = length*animValue + 5 > length ? length : length*animValue + 5;
+            circleMeasure.getSegment(length*animValue,stopD,dst,true);
+            canvas.drawPath(dst,mPaint);
+        }
+//        canvas.drawPath(searchPath,mPaint);
+//        canvas.drawPath(circlePath,mPaint);
+    }
+
+    /**
+     *飞机绕圈 第一种实现
+     * 通过 tan 得值计算出图片旋转的角度，tan 是 tangent 的缩写，
+     * 即中学中常见的正切， 其中tan0是邻边边长，tan1是对边边长，
+     * 而Math中 atan2 方法是根据正切是数值计算出该角度的大小,得到的单位是弧度，所以上面又将弧度转为了角度
+     * @param canvas
+     */
+    private void aroundFly1(Canvas canvas){
         mMatrix.reset();
-
         Path circlePath = new Path();
         mPaint.setColor(Color.CYAN);
         circlePath.addCircle(0,0,80.0f, Path.Direction.CW);
@@ -127,9 +202,14 @@ public class PathMeasureDemo extends View {
         mMatrix.postTranslate(pos[0] -mBitmap.getWidth()/2,pos[1]-mBitmap.getHeight()/2);
         canvas.drawPath(circlePath,mPaint);
         canvas.drawBitmap(mBitmap,mMatrix,mPaint);
+    }
 
-        //第二种实现
-//      矩阵对旋转角度默认为图片的左上角，我们此处需要使用 preTranslate 调整为图片中心。
+    /**
+     *飞机绕圈 第二种实现
+     * 矩阵对旋转角度默认为图片的左上角，我们此处需要使用 preTranslate 调整为图片中心。
+     * @param canvas
+     */
+    private void aroundFly2(Canvas canvas){
         mPaint.setColor(Color.RED);
         Path circlePath2 = new Path();
         circlePath2.addCircle(200,200,80.0f, Path.Direction.CW);
@@ -140,28 +220,12 @@ public class PathMeasureDemo extends View {
         mMatrix.preTranslate(-mBitmap.getWidth()/2,-mBitmap.getHeight()/2);
         canvas.drawPath(circlePath2,mPaint);
         canvas.drawBitmap(mBitmap,mMatrix,mPaint);
-
-
-        //搜索框动画
-        mPaint.setColor(Color.GREEN);
-        mPaint.setStrokeWidth(10.0f);
-        Path searchPath = new Path();
-        searchPath.addCircle(-200,-200,80,Path.Direction.CW);
-        PathMeasure searchMeasure = new PathMeasure(searchPath,false);
-        Path dst = new Path();
-        searchMeasure.getSegment(0,searchMeasure.getLength()*animValue,dst,false);
-        canvas.drawPath(dst,mPaint);
     }
 
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        setMeasuredDimension(MeasureSpec.getSize(widthMeasureSpec),MeasureSpec.getSize(heightMeasureSpec));
-    }
 
 
-    private void aroundFly(){
+    private void aroundFlyAnim(){
         ObjectAnimator animValue = ObjectAnimator.ofFloat(this, "animValue", 0f, 1f);
         animValue.setDuration(10*1000);
         animValue.setInterpolator(new AccelerateDecelerateInterpolator());
